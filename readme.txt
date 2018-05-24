4d86194da563d562d5504ededb561884e3631414
thayalangr <grthayalan18@gmal.com>
2018-05-24 18:54:45 +0530

    initial commit on date may 24

---
+++ readme.txt
@@ -0,0 +1,6 @@
+doodleHub
+
+
+homepage and authentication
+
+registering users
---
+++ script/validation.js
@@ -0,0 +1,7 @@
+var main = function() {
+    $('.drop').click(function() {
+      $('.dropdown-menu').toggle();
+    });
+  }
+  
+  $(document).ready(main);
